7a7103294e2b2d1363d239a60cea1c7621ef2be0
MR-JAGANMOHAN <107793234+MR-JAGANMOHAN@users.noreply.github.com>
2023-04-22 11:35:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jagan:753753753@cluster0.zisdn.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Mdiskconverter")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Mdiskconverters")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
